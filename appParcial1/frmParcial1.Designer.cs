e1c37f67a713a6f66e86fa5368d3b4212d19ae60
Deyssi Vergara <deyssiyuliana@gmail.com>
2023-08-29 15:53:34 -0500

    Se termina plantilla

---
+++ appParcial1/frmParcial1.Designer.cs
@@ -0,0 +1,95 @@
+﻿
+namespace appParcial1
+{
+    partial class frmParcial1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textFloat = new System.Windows.Forms.TextBox();
+            this.textInt2 = new System.Windows.Forms.TextBox();
+            this.textInt1 = new System.Windows.Forms.TextBox();
+            this.MensajError = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textFloat
+            // 
+            this.textFloat.Location = new System.Drawing.Point(210, 50);
+            this.textFloat.Name = "textFloat";
+            this.textFloat.Size = new System.Drawing.Size(100, 20);
+            this.textFloat.TabIndex = 0;
+            // 
+            // textInt2
+            // 
+            this.textInt2.Location = new System.Drawing.Point(210, 183);
+            this.textInt2.Name = "textInt2";
+            this.textInt2.Size = new System.Drawing.Size(100, 20);
+            this.textInt2.TabIndex = 1;
+            // 
+            // textInt1
+            // 
+            this.textInt1.Location = new System.Drawing.Point(210, 116);
+            this.textInt1.Name = "textInt1";
+            this.textInt1.Size = new System.Drawing.Size(100, 20);
+            this.textInt1.TabIndex = 2;
+            // 
+            // MensajError
+            // 
+            this.MensajError.BackColor = System.Drawing.SystemColors.GrayText;
+            this.MensajError.Location = new System.Drawing.Point(26, 409);
+            this.MensajError.Multiline = true;
+            this.MensajError.Name = "MensajError";
+            this.MensajError.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.MensajError.Size = new System.Drawing.Size(341, 29);
+            this.MensajError.TabIndex = 3;
+            // 
+            // frmParcial1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(379, 450);
+            this.Controls.Add(this.MensajError);
+            this.Controls.Add(this.textInt1);
+            this.Controls.Add(this.textInt2);
+            this.Controls.Add(this.textFloat);
+            this.Name = "frmParcial1";
+            this.Text = "Form1";
+            this.TransparencyKey = System.Drawing.Color.SteelBlue;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textFloat;
+        private System.Windows.Forms.TextBox textInt2;
+        private System.Windows.Forms.TextBox textInt1;
+        private System.Windows.Forms.TextBox MensajError;
+    }
+}
+
---
+++ appParcial1/frmParcial1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using libOpe;
+
+namespace appParcial1
+{
+    public partial class frmParcial1 : Form
+    {
+        public frmParcial1()
+        {
+            InitializeComponent();
+        }
+
+        #region Methots propios
+        private void Mensaje(string texto)
+        {
+            this.MensajError.Text = texto;
+        }
+
+        private void Limpiar()
+        {
+
+            //this.comboBoxTipoEst.SelectedIndex = 0;
+            //foreach (Control c in this.Controls)
+            //{
+            //    if (c is TextBox)
+            //    {
+            //        c.Text = string.Empty;
+            //    }
+            //}
+            //this.groupBoxResults.Visible = false;
+            //this.comboBoxTipoEst.Focus();
+        }
+        #endregion
+
+        #region Eventos del objeto
+        private void ButtonTerminar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void buttonProcesar_Click(object sender, EventArgs e)
+        {
+            float float1;
+            int int1, int2;
+
+            try
+            {
+                Mensaje("");
+                //Captura de datos
+
+                tipoE = this.comboBoxTipoEst.SelectedIndex;
+                if (tipoE <= 0)
+                {
+                    Mensaje("Tipo de estudiante no válido");
+                    this.comboBoxTipoEst.Focus();
+                    return;
+                }
+                int1 = Convert.ToInt32(this.textInt1.Text);
+                int2 = Convert.ToInt32(this.textInt2.Text);
+               
+
+                //Se crea el objeto
+                clsOpe oP = new clsOpe();
+                //Enviar informaciòn al objeto
+                oP.tipoEstudiante = tipoE;
+                oP.Promedio = pNota;
+                //Invocar el metodo y tratamiento de error
+                if (!oP.hallarPago())
+                {
+                    Mensaje(oP.Error);
+                    oP = null;
+                    return;
+                }
+                //Recuperar inforación
+                this.textBoxNumCred.Text = oP.numCreditos.ToString();
+                this.txtVrCred.Text = oP.ValorCred.ToString();
+                this.txtDscto.Text = oP.Descuento.ToString();
+                this.txtVrPagar.Text = oP.valorPago.ToString();
+                this.groupBoxResults.Visible = true;
+                oP = null;
+            }
+            catch (Exception ex)
+            {
+                Mensaje(ex.Message);
+            }
+        }
+        #endregion
+    }
+}
